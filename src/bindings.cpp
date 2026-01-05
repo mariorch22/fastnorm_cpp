20c5b8eee76d7a3824bd757cea6799045252cb7f
mariorch22 <marioraach01@gmail.com>
2026-01-05 19:36:39 +0100

    updated version number

--- src/bindings.cpp
+++ src/bindings.cpp
@@ -7,6 +7,7 @@ namespace py = pybind11;
 
 PYBIND11_MODULE(fastnorm, m) {
     m.doc() = "Mein schnelles RMSNorm Modul"; 
+    m.attr("__version__") = "0.1.3";
     m.def("rmsnorm", &rms_norm, "Berechnet RMSNorm für einen Vektor");
     m.def("rmsnorm_numpy", &rms_norm_numpy, "Berechnet RMSNorm direkt auf NumPy-Speicher (Zero-Copy)");
 }
